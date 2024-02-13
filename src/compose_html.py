4cbbe90804a1181418fb1f24e368dbcb11894943
Reuben Gann <reubengann@gmail.com>
2024-02-13 12:02:13 -0500

    Keep periods inside text

--- src/compose_html.py
+++ src/compose_html.py
@@ -78,35 +78,21 @@ def make_html_from_lines(input_contents: str) -> str:
             checklist_items.clear()
         else:
             p = html.new_tag("p")
-            for part in parse_line(line):
+            for part in parse_line(line, line_no):
                 if part.part_type == PartType.TEXT:
                     p.append(part.part)
                 elif part.part_type == PartType.CHECK_ITEM:
-                    if "|" not in part.part:
-                        print(f"Invalid collectible {part.part} on line {line_no}")
+                    if part.tag_name not in decl_map:
+                        print(f"Invalid collectible {part.tag_name} on line {line_no}")
+                    else:
                         label_tag = html.new_tag("label")
                         checkbox_tag = html.new_tag("input", type="checkbox")
                         label_tag.string = part.part
                         p.append(checkbox_tag)
                         p.append(label_tag)
-                    else:
-                        tag_name, content = part.part.split("|", 1)
-                        tag_name = tag_name.strip()
-                        content = content.strip()
-                        list_content = None
-                        if "|" in content:
-                            content, list_content = content.split("|", 1)
-                        if tag_name not in decl_map:
-                            print(f"Invalid collectible {tag_name} on line {line_no}")
-                        else:
-                            label_tag = html.new_tag("label")
-                            checkbox_tag = html.new_tag("input", type="checkbox")
-                            label_tag.string = content
-                            p.append(checkbox_tag)
-                            p.append(label_tag)
-                            checklist_items.setdefault(tag_name, []).append(
-                                list_content or content
-                            )
+                        checklist_items.setdefault(part.tag_name, []).append(
+                            part.rollup_name or part.part
+                        )
             body_tag.append(p)
     return html.prettify()
 
@@ -114,13 +100,23 @@ def make_html_from_lines(input_contents: str) -> str:
 class LinePart:
     part_type: PartType
     part: str
+    tag_name: str | None
+    rollup_name: str | None
 
-    def __init__(self, part_type: PartType, part: str) -> None:
+    def __init__(
+        self,
+        part_type: PartType,
+        part: str,
+        rollup_name: str | None = None,
+        tag_name: str | None = None,
+    ) -> None:
         self.part_type = part_type
         self.part = part
+        self.rollup_name = rollup_name
+        self.tag_name = tag_name
 
 
-def parse_line(line: str) -> list[LinePart]:
+def parse_line(line: str, line_no: int) -> list[LinePart]:
     parts: list[LinePart] = []
     line = line.strip()
     remainder = line
@@ -129,7 +125,13 @@ def parse_line(line: str) -> list[LinePart]:
         parts.append(LinePart(PartType.TEXT, part.strip()))
         if "]" in remainder:
             part, remainder = remainder.split("]", maxsplit=1)
-            parts.append(LinePart(PartType.CHECK_ITEM, part.strip()))
+            lp = parse_checklist_item(part, line_no)
+            if lp is None:
+                continue
+            if remainder.startswith("."):
+                lp.part += "."
+                remainder = remainder[1:]
+            parts.append(lp)
     parts.append(LinePart(PartType.TEXT, remainder.strip()))
     return parts
 
@@ -156,3 +158,16 @@ def parse_declaration(line: str, line_no: int) -> Declaration | None:
         name, remainder = remainder.split("}", 1)
         parts.append(name)
     return Declaration(*parts)
+
+
+def parse_checklist_item(s: str, line_no: int) -> LinePart | None:
+    if "|" not in s:
+        print(f"Invalid collectible {s} on line {line_no}")
+        return None
+    tag_name, content = s.split("|", 1)
+    tag_name = tag_name.strip()
+    content = content.strip()
+    list_content = None
+    if "|" in content:
+        content, list_content = content.split("|", 1)
+    return LinePart(PartType.CHECK_ITEM, content, list_content, tag_name)
